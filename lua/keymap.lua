438f107aac7fe0b47ac7db0de84fbb1b12639058
Ayub Malik <ayub.malik@gmail.com>
2022-11-07 23:05:54 +0000

    feat: update terminal keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -9,23 +9,31 @@ function map(mode, lhs, rhs)
 end
 
 -- Normal
-map("n", "<F2>", ":set relativenumber!<cr>")
-map("n", "<F3>", ":set hlsearch!<cr>")
-map("n", "<C-s>", ":update<cr>")
-map("n", "<A-j>", ":m .+1<cr>")
-map("n", "<A-k>", ":m .-2<cr>")
+map("n", "<F2>", ":set relativenumber!<CR>")
+map("n", "<F3>", ":set hlsearch!<CR>")
+map("n", "<C-s>", ":update<CR>")
+map("n", "<A-j>", ":m .+1<CR>")
+map("n", "<A-k>", ":m .-2<CR>")
 
 -- Insert
 map("i", "kj", "<esc>")
-map("i", "<C-s>", "<esc>:update<cr>a")
+map("i", "<C-s>", "<esc>:update<CR>a")
 map("i", "<C-d>", "<esc>lxi")
 
 -- Visual
 map("v", "<", "<gv")
 map("v", ">", ">gv")
-map("v", "<A-j>", ":m '>+1<cr>gv=gv")
-map("v", "<A-k>", ":m '<-2<cr>gv=gv")
+map("v", "<A-j>", ":m '>+1<CR>gv=gv")
+map("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
+-- Terminal
+map("n","<leader>t", ":split | resize 16 | terminal<CR>")
+map("t", "<esc>", "<C-\\><C-n>")
+
+vim.api.nvim_create_autocmd(
+  { "TermOpen" },
+  { pattern = {"*"}, command = "startinsert" }
+)
 
 -- Telescope
 -- see https://github.com/nvim-telescope/telescope.nvim#getting-started
