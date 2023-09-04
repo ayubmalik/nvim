5a0e654c51fe0ed911cd9e8ff824ad6f0e8ece18
Ayub Malik <ayub.malik@gmail.com>
2023-09-04 21:57:59 +0100

    Tidy init.lua

--- init.lua
+++ init.lua
@@ -1,15 +1,12 @@
-require "plugins"
+
 require "options"
+require "plugins"
+require "colorscheme"
 require "keymap"
 require "colorscheme"
 require "completion"
 require "lsp-config"
 
-table.unpack = table.unpack or unpack -- 5.1 compatibility see https://github.com/hrsh7th/nvim-cmp/issues/1017
+-- 5.1 compatibility see https://github.com/hrsh7th/nvim-cmp/issues/1017
+table.unpack = table.unpack or unpack 
 
--- using fzf.lua instead
---require("telescope").setup {
---  defaults = {
---    initial_mode = "insert"
---  }
---}
