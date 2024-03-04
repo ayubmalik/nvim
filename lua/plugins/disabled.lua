6c0b4e0ebf22f7f583d46f5913bb630d134b5d49
Ayub Malik <ayub.malik@gmail.com>
2024-03-04 19:24:43 +0000

    Disbale Flit and update

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
-  -- disable noice
   { "folke/noice.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
+  { "echasnovski/mini.files", enabled = false },
 }
