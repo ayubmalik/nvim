7122b5e8e8e1b4ccb63d5f65e5e78c34d3d21b2d
Ayub Malik <ayub.malik@gmail.com>
2024-03-19 21:46:59 +0000

    disable spectre

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,9 +1,9 @@
 return {
-  { "folke/noice.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "echasnovski/mini.files", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/noice.nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
-  { "catppuccin/nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
   { "nvim-telescope/telescope-fzf-native.nvim ", enabled = false },
 }
