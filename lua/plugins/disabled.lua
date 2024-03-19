d3afb3daf1853c590417f80d60311c100ae9c844
Ayub Malik <ayub.malik@gmail.com>
2024-03-19 21:33:56 +0000

    disable indentscope

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,7 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "echasnovski/mini.files", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "nvim-telescope/telescope-fzf-native.nvim ", enabled = false },
