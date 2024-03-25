1b66eb0170750771d5f5e5a2b1a80a42ceca843b
Ayub Malik <ayub.malik@gmail.com>
2024-03-25 19:05:08 +0000

    Mini pairs try fic quoutes

---
+++ lua/plugins/minipairs.lua
@@ -0,0 +1,26 @@
+-- customize mini.pairs
+
+return {
+  {
+    "echasnovski/mini.pairs",
+    opts = {
+      -- In which modes mappings from this `config` should be created
+      modes = { insert = true, command = false, terminal = false },
+      mappings = {
+        ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+        ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+        ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+
+        [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+        ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+        ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+        -- ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+        -- set double quote to same as single
+        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%a\\].", register = { cr = false } },
+        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+      },
+    },
+  },
+}
