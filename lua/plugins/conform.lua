9cf60551161ff8c9c5173c33e6688774d02fc1e5
Ayub Malik <ayub.malik@gmail.com>
2025-04-11 22:02:14 +0100

    conform cpp

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -34,6 +34,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         go = { 'gofmt' },
+        cpp = { 'clang_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
