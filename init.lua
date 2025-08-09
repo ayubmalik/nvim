74e37a54ea7e4f702439018f33ef80cfae74aea5
Ayub Malik <ayub.malik@coachusa.com>
2025-08-09 20:26:57 +0100

    move install lazy to lazyvim.lua

--- init.lua
+++ init.lua
@@ -3,36 +3,8 @@ vim.g.have_nerd_font = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-       local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-      require('lazy').setup {
-  spec = {
-    -- import your plugins
-    { import = 'plugins' },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "tokyonight-night" } },
-  -- automatically check for plugin updates
-  checker = { enabled = false },
-  ui = {
-    border = 'rounded',
-    backdrop = 0
-  }
-}
-
 require('options')
 require('keymaps')
 require('autocmds')
-require("lsp")
-
+require('lsp')
+require('lazyvim')
