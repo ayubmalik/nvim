2aa8c2323caa01fa8a87b7142755f8790bfc2093
Ayub Malik <ayub.malik@coachusa.com>
2025-08-13 12:50:18 +0100

    Lazy git 2

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,9 +15,12 @@ return {
     },
     -- optional for floating window border decoration
     dependencies = {
-      'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
-      'ibhagwan/fzf-lua', -- optional
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Open lazy git' },
     },
   },
   {
