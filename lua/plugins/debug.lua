e42ff70b09baa37dfa16299846cd7a65f0d523bf
Ayub Malik <ayub.malik@gmail.com>
2026-06-23 21:26:46 +0100

    feat: disable debug.lus

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,131 +1,132 @@
 -- [[ start debug ]]
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+	-- NOTE: Yes, you can install new plugins here!
+	'mfussenegger/nvim-dap',
+	-- NOTE: And you can specify dependencies as well
+	dependencies = {
+		-- Creates a beautiful debugger UI
+		'rcarriga/nvim-dap-ui',
 
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
+		-- Required dependency for nvim-dap-ui
+		'nvim-neotest/nvim-nio',
 
-    --
-    'leoluz/nvim-dap-go',
-  },
-  lazy = true,
-  keys = function(_, keys)
-    local dap = require 'dap'
-    local dapui = require 'dapui'
-    return {
-      -- Basic debugging keymaps, feel free to change to your liking!
-      { '<F9>', dap.continue, desc = 'Debug: Start/Continue' },
-      { '<F7>', dap.step_into, desc = 'Debug: Step Into' },
-      { '<F8>', dap.step_over, desc = 'Debug: Step Over' },
-      { '<s-F8>', dap.step_out, desc = 'Debug: Step Out' },
-      { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
-      {
-        '<leader>B',
-        function()
-          dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-        end,
-        desc = 'Debug: Set Breakpoint',
-      },
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-      { '<leader>du', dapui.toggle, desc = 'Debug: See last session result.' },
-      unpack(keys),
-    }
-  end,
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+		--
+		'leoluz/nvim-dap-go',
+	},
+	enabled = false,
+	lazy = true,
+	keys = function(_, keys)
+		local dap = require 'dap'
+		local dapui = require 'dapui'
+		return {
+			-- Basic debugging keymaps, feel free to change to your liking!
+			{ '<F9>',      dap.continue,          desc = 'Debug: Start/Continue' },
+			{ '<F7>',      dap.step_into,         desc = 'Debug: Step Into' },
+			{ '<F8>',      dap.step_over,         desc = 'Debug: Step Over' },
+			{ '<s-F8>',    dap.step_out,          desc = 'Debug: Step Out' },
+			{ '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+			{
+				'<leader>B',
+				function()
+					dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+				end,
+				desc = 'Debug: Set Breakpoint',
+			},
+			-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+			{ '<leader>du', dapui.toggle, desc = 'Debug: See last session result.' },
+			unpack(keys),
+		}
+	end,
+	config = function()
+		local dap = require 'dap'
+		local dapui = require 'dapui'
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      controls = {
-        element = 'repl',
-        enabled = true,
-        -- icons = {
-        --   pause = '⏸',
-        --   play = '▶',
-        --   step_into = '⏎',
-        --   step_over = '⏭',
-        --   step_out = '⏮',
-        --   step_back = 'b',
-        --   run_last = '▶▶',
-        --   terminate = '⏹',
-        --   disconnect = '⏏',
-        -- },
-      },
-      element_mappings = {},
-      expand_lines = true,
-      floating = {
-        border = 'single',
-        mappings = {
-          close = { 'q', '<Esc>' },
-        },
-      },
-      force_buffers = true,
-      icons = {
-        expanded = '▾',
-        collapsed = '▸',
-        current_frame = '*',
-      },
-      layouts = {
-        {
-          elements = {
-            {
-              id = 'scopes',
-              size = 0.25,
-            },
-            {
-              id = 'breakpoints',
-              size = 0.25,
-            },
-            {
-              id = 'stacks',
-              size = 0.25,
-            },
-            {
-              id = 'watches',
-              size = 0.25,
-            },
-          },
-          position = 'left',
-          size = 40,
-        },
-        {
-          elements = { {
-            id = 'console',
-            size = 0.7,
-          }, {
-            id = 'repl',
-            size = 0.3,
-          } },
-          position = 'bottom',
-          size = 10,
-        },
-      },
-      mappings = {
-        edit = 'e',
-        expand = { '<CR>', '<2-LeftMouse>' },
-        open = 'o',
-        remove = 'd',
-        repl = 'r',
-        toggle = 't',
-      },
-      render = {
-        indent = 1,
-        max_value_lines = 100,
-      },
-    }
+		-- Dap UI setup
+		-- For more information, see |:help nvim-dap-ui|
+		dapui.setup {
+			-- Set icons to characters that are more likely to work in every terminal.
+			--    Feel free to remove or use ones that you like more! :)
+			--    Don't feel like these are good choices.
+			controls = {
+				element = 'repl',
+				enabled = true,
+				-- icons = {
+				--   pause = '⏸',
+				--   play = '▶',
+				--   step_into = '⏎',
+				--   step_over = '⏭',
+				--   step_out = '⏮',
+				--   step_back = 'b',
+				--   run_last = '▶▶',
+				--   terminate = '⏹',
+				--   disconnect = '⏏',
+				-- },
+			},
+			element_mappings = {},
+			expand_lines = true,
+			floating = {
+				border = 'single',
+				mappings = {
+					close = { 'q', '<Esc>' },
+				},
+			},
+			force_buffers = true,
+			icons = {
+				expanded = '▾',
+				collapsed = '▸',
+				current_frame = '*',
+			},
+			layouts = {
+				{
+					elements = {
+						{
+							id = 'scopes',
+							size = 0.25,
+						},
+						{
+							id = 'breakpoints',
+							size = 0.25,
+						},
+						{
+							id = 'stacks',
+							size = 0.25,
+						},
+						{
+							id = 'watches',
+							size = 0.25,
+						},
+					},
+					position = 'left',
+					size = 40,
+				},
+				{
+					elements = { {
+						id = 'console',
+						size = 0.7,
+					}, {
+						id = 'repl',
+						size = 0.3,
+					} },
+					position = 'bottom',
+					size = 10,
+				},
+			},
+			mappings = {
+				edit = 'e',
+				expand = { '<CR>', '<2-LeftMouse>' },
+				open = 'o',
+				remove = 'd',
+				repl = 'r',
+				toggle = 't',
+			},
+			render = {
+				indent = 1,
+				max_value_lines = 100,
+			},
+		}
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-  end,
+		dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+		dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+		dap.listeners.before.event_exited['dapui_config'] = dapui.close
+	end,
 }
