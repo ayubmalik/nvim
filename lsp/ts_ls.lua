34d8657087146524f213727f13557de8bbade8a2
Ayub Malik <ayub.malik@gmail.com>
2026-07-02 23:45:33 +0100

    chore: remove unused files

--- lsp/ts_ls.lua
+++
@@ -1,6 +0,0 @@
-return {
-  cmd = { 'typescript-language-server', '--stdio' },
-  filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
-  root_markers = { 'tsconfig.json', 'package.json', '.git' },
-  single_file_support = true,
-}
